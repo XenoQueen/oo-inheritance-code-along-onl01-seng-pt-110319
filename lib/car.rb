03020f868d7d007633405305a69ec16421dc3030
Maika Kailani <queenmab051@gmail.com>
2019-12-15 17:24:46 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -4,4 +4,9 @@ class Vehicle
   
   attr_accessor :wheel_size, :wheel_number
   
+  def initialize(wheel_number, wheel_size)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
   
